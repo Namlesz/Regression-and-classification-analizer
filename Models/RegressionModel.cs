a4a069e5a7172a563c2fa54c56030919f2b5d4ce
Adam Wiśniewski <nejmlesz@gmail.com>
2022-12-04 15:40:28 +0100

    Add regresion table, apply style

---
+++ Models/RegressionModel.cs
@@ -0,0 +1,9 @@
+namespace EksploracjaDanych.Models;
+
+public class RegressionModel
+{
+    public double MAE { get; set; }
+    public double MSE { get; set; }
+    public double RMSE { get; set; }
+    public double MAPE { get; set; }
+}
